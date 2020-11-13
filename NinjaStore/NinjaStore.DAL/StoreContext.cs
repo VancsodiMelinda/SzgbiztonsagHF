0760a627a05e114d999dc388b2b0b1f6628cfb36
filipinyidaniel <filipinyi.daniel@gmail.com>
2020-11-13 23:18:57 +0100

    Added DbContext

---
+++ NinjaStore/NinjaStore.DAL/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NinjaStore.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NinjaStore.DAL
+{
+	public class StoreContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<CaffMetadata> CaffMetadata { get; set; }
+		public DbSet<CaffFile> CaffFiles { get; set; }
+		public DbSet<Comment> Comments { get; set; }
+
+		public StoreContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+}
