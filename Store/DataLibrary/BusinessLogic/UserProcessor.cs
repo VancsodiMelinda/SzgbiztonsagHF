2c36220d63c850c7e230422f16f00da141426b28
Blaskó Gergő <blasko.gergo.hun@gmail.com>
2020-11-13 12:03:30 +0100

    Logic integrated to the Store.

---
+++ Store/DataLibrary/BusinessLogic/UserProcessor.cs
@@ -0,0 +1,39 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class UserProcessor
+    {
+        public static int CreateUser(int UserId, string firstName,
+            string lastName, string emailAddress, string password, string role)
+        {
+            UserModel data = new UserModel
+            {
+                UserName = UserId,
+                FirstName = firstName,
+                EmailAddress = emailAddress,
+                Password = password,
+                Role = role
+            };
+
+            string sql = @"insert into User (Username, Name, Email, PasswordHash, Role)
+                           values (@Username, @LastName, @EmailAddress, @Password, @Role);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<UserModel> LoadEmployees()
+        {
+            string sql = @"select Username, Name, Email, PasswordHash, Role
+                           from User;";
+
+            return SqlDataAccess.LoadData<UserModel>(sql);
+        }
+    }
+}
