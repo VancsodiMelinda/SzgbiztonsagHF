d6c0d92fc1c2c2bb8ff88c74a2cc8d6ae07e71dd
filipinyidaniel <filipinyi.daniel@gmail.com>
2020-11-15 19:37:01 +0100

    Created NinjaStore.BLL project

---
+++ NinjaStore/NinjaStore.BLL/IStoreLogic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NinjaStore.BLL
+{
+	public interface IStoreLogic
+	{
+	}
+}
---
+++ NinjaStore/NinjaStore.BLL/StoreLogic.cs
@@ -0,0 +1,20 @@
+﻿using NinjaStore.DAL;
+using NinjaStore.Parser.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NinjaStore.BLL
+{
+	public class StoreLogic : IStoreLogic
+	{
+		private StoreContext _storeContext;
+		private IParserService _parserService;
+
+		public StoreLogic(StoreContext storeContext, IParserService parserService)
+		{
+			_storeContext = storeContext;
+			_parserService = parserService;
+		}
+	}
+}
