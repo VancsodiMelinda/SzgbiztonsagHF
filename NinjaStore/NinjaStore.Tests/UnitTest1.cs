c7042e52dc702dbfffeb4bacee892b1d6be265de
vancsodimeli <vancsodimeli@gmail.com>
2020-11-18 15:13:51 +0100

    added project for unit testing

---
+++ NinjaStore/NinjaStore.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using NinjaStore.Pages.Files;
+using Microsoft.EntityFrameworkCore;
+using NinjaStore.DAL;
+using Moq;
+
+namespace NinjaStore.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+            //var mockStoreContext = new Mock<StoreContext>(optionsBuilder.Options);
+            
+            //var indexModel = new IndexModel(context);
+        }
+    }
+}
