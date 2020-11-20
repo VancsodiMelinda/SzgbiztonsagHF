588d56bf98afb72859f401e8ba755b806e197117
vancsodimeli <vancsodimeli@gmail.com>
2020-11-20 14:33:35 +0100

    unit tests

--- NinjaStore/NinjaStore.Tests/UnitTests/DALtests.cs
+++ NinjaStore/NinjaStore.Tests/UnitTests/DALtests.cs
@@ -5,14 +5,19 @@ using Microsoft.EntityFrameworkCore;
 using NinjaStore.DAL;
 using Moq;
 
-namespace NinjaStore.Tests
+namespace NinjaStore.Tests.UnitTests
 {
-    public class UnitTest1
+    public class DALtests
     {
         [Fact]
-        public void Test1()
+        public async void Test1()
         {
-            //var optionsBuilder = new DbContextOptionsBuilder<StoreContext>();
+            using (var db = new StoreContext(Utilities.TestDbContextOptions()))
+            {
+
+            }
+
+            //var optionsBuilder = new DbContextOptionsBuilder<StoreContext>().UseInMemoryDatabase("InMemoryDb");
             //var mockStoreContext = new Mock<StoreContext>(optionsBuilder.Options);
             
             //var indexModel = new IndexModel(context);
---
+++ NinjaStore/NinjaStore.Tests/Utilities/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NinjaStore.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NinjaStore.Tests
+{
+    public static class Utilities
+    {
+        #region snippet1
+        public static DbContextOptions<StoreContext> TestDbContextOptions()
+        {
+            // Create a new service provider to create a new in-memory database.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance using an in-memory database and 
+            // IServiceProvider that the context should resolve all of its 
+            // services from.
+            var builder = new DbContextOptionsBuilder<StoreContext>()
+                .UseInMemoryDatabase("InMemoryDb")
+                .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+        #endregion
+    }
+}
