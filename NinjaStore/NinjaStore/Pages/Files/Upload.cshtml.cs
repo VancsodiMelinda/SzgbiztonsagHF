34087104ae46adb9720d4520374db95774f4f4e9
Tóthpál Csilla <tothpalcsilla@gmail.com>
2020-11-25 00:14:27 +0100

    Upload - validate

--- NinjaStore/NinjaStore/Pages/Files/Upload.cshtml.cs
+++ NinjaStore/NinjaStore/Pages/Files/Upload.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,12 +23,14 @@ namespace NinjaStore.Pages.Files
         }
 
         [BindProperty]
+        [Required(ErrorMessage ="The name of the file is required.")]
         public string FileName { get; set; }
 
         [BindProperty]
         public string Description { get; set; }
 
         [BindProperty]
+        [Required(ErrorMessage = "Please select a file.")]
         public IFormFile NewFile { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
@@ -44,7 +47,7 @@ namespace NinjaStore.Pages.Files
                 
                 // TODO Csilla: username
                 string savedFileId = await _logic.UploadFileAsync("Csilla", FileName, Description, preview);
-                return RedirectToPage("./Index");
+                return RedirectToPage("./Details", new { id = savedFileId });
             }
         }
     }
