b6c6ef83544d4749ed17b12afd066b324523cf5f
vancsodimeli <vancsodimeli@gmail.com>
2020-11-26 10:26:57 +0100

    more page model tests

---
+++ NinjaStore/NinjaStore.Tests/UnitTests/Files/FilesIndexTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using NinjaStore.DAL;
+using NinjaStore.BLL;
+using Moq;
+using System.Threading.Tasks;
+using NinjaStore.Pages.Files;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NinjaStore.DAL.Models;
+using NinjaStore.Tests.Helper;
+using NinjaStore.Parser.Services;
+
+namespace NinjaStore.Tests.UnitTests.Files.Index
+{
+    public class FilesIndexTests
+    {
+        static DbContextOptions<StoreContext> options { get; set; }
+        static CaffFile testData;
+
+        static FilesIndexTests()
+        {
+            Helper.Helper helper = new Helper.Helper();
+            options = helper.options;
+            testData = helper.testData;
+        }
+
+        // METHOD: OnGetAsync
+        /*
+        // the OnGetAsync method doesn't return anything
+        [Fact]
+        public async Task OnGetAsync_NoReturn()
+        {
+
+        }
+        */
+    }
+}
---
+++ NinjaStore/NinjaStore.Tests/UnitTests/Files/FilesUploadTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using NinjaStore.DAL;
+using NinjaStore.BLL;
+using Moq;
+using System.Threading.Tasks;
+using NinjaStore.Pages.Files;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NinjaStore.DAL.Models;
+using NinjaStore.Tests.Helper;
+using NinjaStore.Parser.Services;
+
+namespace NinjaStore.Tests.UnitTests.Files.Upload
+{
+    public class FilesUploadTests
+    {
+        static DbContextOptions<StoreContext> options { get; set; }
+        static CaffFile testData;
+
+        static FilesUploadTests()
+        {
+            Helper.Helper helper = new Helper.Helper();
+            options = helper.options;
+            testData = helper.testData;
+        }
+
+        // METHOD: OnPostAsync
+        [Fact]
+        public async Task OnPostAsync_ReturnsPageResult_WhenModelStateIsInvalid()
+        {
+            // mock parser service interface
+            Mock<IParserService> mockIParserService = new Mock<IParserService>();
+
+            // test
+            using (var context = new StoreContext(options))
+            {
+                StoreLogic storeLogic = new StoreLogic(context, mockIParserService.Object);
+                var uploadModel = new UploadModel(storeLogic);
+                uploadModel.ModelState.AddModelError("test", "test");
+                var result = await uploadModel.OnPostAsync();
+
+                Assert.IsType<PageResult>(result);
+            }
+        }
+
+        /*
+        [Fact]
+        public async Task OnPostAsync_ReturnsRedirectToPageResult_WhenModelStateIsInvalid()
+        {
+            // mock parser service interface
+            Mock<IParserService> mockIParserService = new Mock<IParserService>();
+
+            // test
+            using (var context = new StoreContext(options))
+            {
+                StoreLogic storeLogic = new StoreLogic(context, mockIParserService.Object);
+                var uploadModel = new UploadModel(storeLogic);
+
+                var result = await uploadModel.OnPostAsync();
+
+                Assert.IsType<RedirectToPageResult>(result);
+            }
+        }
+        */
+    }
+}
