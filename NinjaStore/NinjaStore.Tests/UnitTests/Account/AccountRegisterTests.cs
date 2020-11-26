8115e4b5f59184bdd1c273a571a3ecba56a02b6d
vancsodimeli <vancsodimeli@gmail.com>
2020-11-26 13:53:32 +0100

    added outlines for comments create, delete tests and account register tests

---
+++ NinjaStore/NinjaStore.Tests/UnitTests/Account/AccountRegisterTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using NinjaStore.DAL;
+using NinjaStore.BLL;
+using Moq;
+using System.Threading.Tasks;
+using NinjaStore.Pages.Account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NinjaStore.DAL.Models;
+using NinjaStore.Tests.Helper;
+using NinjaStore.Parser.Services;
+
+namespace NinjaStore.Tests.UnitTests.Account
+{
+    public class AccountRegisterTests
+    {
+        static DbContextOptions<StoreContext> options { get; set; }
+        static CaffFile testData;
+
+        static AccountRegisterTests()
+        {
+            Helper.Helper helper = new Helper.Helper();
+            options = helper.options;
+            testData = helper.testData;
+        }
+
+        // METHOD: OnGet
+        [Fact]
+        public async Task OnGet_ReturnsPageResult()
+        {
+            /*
+            Mock<IStoreLogic> mockIStoreLogic = new Mock<IStoreLogic>();
+            var registerModel = new RegisterModel(mockIStoreLogic.Object);
+
+            string id = null;
+            var result = await deleteModel.OnGetAsync(id);
+
+            Assert.IsType<NotFoundResult>(result);
+            */
+
+            throw new NotImplementedException();
+        }
+
+        // METHOD: OnPostAsync
+        [Fact]
+        public async Task OnPostAsync_ReturnsPageResult_WhenModelIsInvalid()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task OnPostAsync_ReturnsRedirectToPageResult_WhenUserCreationSucceeded()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task OnPostAsync_ReturnsPageResult_WhenUserCreatonFailed()
+        {
+            // ezt egyáltalán lehet tesztelni?
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NinjaStore/NinjaStore.Tests/UnitTests/Comments/CommentsDeleteTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using NinjaStore.DAL;
+using NinjaStore.BLL;
+using Moq;
+using System.Threading.Tasks;
+using NinjaStore.Pages.Comments;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NinjaStore.DAL.Models;
+using NinjaStore.Tests.Helper;
+using NinjaStore.Parser.Services;
+
+namespace NinjaStore.Tests.UnitTests.Comments.Delete
+{
+    public class CommentsDeleteTests
+    {
+        static DbContextOptions<StoreContext> options { get; set; }
+        static CaffFile testData;
+
+        static CommentsDeleteTests()
+        {
+            Helper.Helper helper = new Helper.Helper();
+            options = helper.options;
+            testData = helper.testData;
+        }
+
+        // METHOD: OnPostAsync
+        [Fact]
+        public async Task OnPostAsync_ReturnsNotFoundResult_WhenIdIsNull()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task OnPostAsync_ReturnsNotFoundResult_WhenCommentIsNull()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public async Task OnPostAsync_ReturnsRedirectToPageResult_WhenCommentIsDeleted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
