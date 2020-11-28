4b786e2ee5d486368d37cff816c96bfea51d93a3
vancsodimeli <vancsodimeli@gmail.com>
2020-11-28 12:50:38 +0100

    start comments test

--- NinjaStore/NinjaStore.Tests/UnitTests/Comments/CommentsCreateTests.cs
+++ NinjaStore/NinjaStore.Tests/UnitTests/Comments/CommentsCreateTests.cs
@@ -14,6 +14,7 @@ using NinjaStore.DAL.Models;
 using NinjaStore.Tests.Helper;
 using NinjaStore.Parser.Services;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
 
 namespace NinjaStore.Tests.UnitTests.Comments.Create
 {
@@ -32,14 +33,68 @@ namespace NinjaStore.Tests.UnitTests.Comments.Create
         // METHOD: OnPostAsync
         [Fact]
         public async Task OnPostAsync_ReturnsPageResult_WhenModelIsInvalid()
-        {
-            throw new NotImplementedException();
+        {    
+            // mock
+            Mock<IParserService> mockIParserService = new Mock<IParserService>();
+            Mock<ILogger<CreateModel>> mockILogger = new Mock<ILogger<CreateModel>>();
+
+            List<User> usersList = new List<User> { new User() };
+            var mockUserManager = MockUserManager<User>(usersList);
+
+            //var yourMockOfUserManager = new Mock<UserManager<User>>();
+            //yourMockOfUserManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).Returns(() => null);
+
+            //Mock<IUserStore<User>> mockIUserStore = new Mock<IUserStore<User>>();
+            //var userManager = new UserManager(mockIUserStore.Object, null, null, null, null, null, null, null, null);
+            //Mock<UserManager<User>> mockUserManager = new Mock<UserManager<User>>();
+
+            // test
+            using (var context = new StoreContext(options))
+            {
+                StoreLogic storeLogic = new StoreLogic(context, mockIParserService.Object);
+                var createModel = new CreateModel(storeLogic, mockUserManager.Object, mockILogger.Object, mockILogger.Object);
+                createModel.ModelState.AddModelError("test", "test");
+                string fileID = "testFileID";
+                string commentText = "this is a new comment";
+                var result = await createModel.OnPostAsync(fileID, commentText);
+
+                Assert.IsType<PageResult>(result);
+            }
+            
+            //throw new NotImplementedException();
         }
 
         [Fact]
         public async Task OnPostAsync_ReturnsRedirectToPageResult_WhenModelIsInvalid()
         {
+            /*
+            // mock
+            Mock<IParserService> mockIParserService = new Mock<IParserService>();
+            Mock<ILogger<CreateModel>> mockILogger = new Mock<ILogger<CreateModel>>();
+            Mock<UserManager<User>> mockUserManager = new Mock<UserManager<User>>();
+
+            // test
+            using (var context = new StoreContext(options))
+            {
+                StoreLogic storeLogic = new StoreLogic(context, mockIParserService.Object);
+                var createModel = new CreateModel(storeLogic, mockUserManager.Object, mockILogger.Object, mockILogger.Object);
+            }
+            */
             throw new NotImplementedException();
+        }
+
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>(List<TUser> ls) where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
+            mgr.Object.UserValidators.Add(new UserValidator<TUser>());
+            mgr.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+
+            mgr.Setup(x => x.DeleteAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+            mgr.Setup(x => x.CreateAsync(It.IsAny<TUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<TUser, string>((x, y) => ls.Add(x));
+            mgr.Setup(x => x.UpdateAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+
+            return mgr;
         }
     }
 }
